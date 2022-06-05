dc32f853fe1b5518eed13e423503cf259752a23b
hariharanpk@2703 <pkhariharan2703@gmail.com>
2022-06-05 18:33:26 +0530

    pro

---
+++ src/Components/Hoc.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+export default function Hoc(HocComponent) {
+    return class extends Component{  
+        render(){
+  return (
+    <div>
+        <HocComponent></HocComponent>
+    </div>
+  );}}
+}
